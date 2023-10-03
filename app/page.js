a69e6769745a2d6b543df88afa7da5fe7ba6d8e9
chris.davis5440@gmail.com <chris.davis5440@gmail.com>
2023-10-03 10:45:07 -0400

    Added center artwork

--- app/page.js
+++ app/page.js
@@ -5,15 +5,15 @@ export default function Home() {
 
       <nav></nav>
 
-      <div className='flex items-center justify-center w-full gap-2 mt-10'>
+      <div className='flex items-center justify-center w-full gap-4 mt-10'>
         {/* Left */}
         <div className='w-[600px]'>
           <img src='/8footDesignLeft-1ver4.png' alt='Left Image' />
         </div>
 
         {/* Center */}
-        <div className='w-[600px]'>
-          <img src='/8footDesignLeft-1ver4.png' alt='Center Image' />
+        <div className='w-[259.5px]'>
+          <img src='/3footDesign-1.png' alt='Center Image' />
         </div>
 
         {/* Right */}
