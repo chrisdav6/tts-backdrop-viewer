c3aea904f5da7f6da41b324c95cbcc5fdebb153c
chris.davis5440@gmail.com <chris.davis5440@gmail.com>
2023-10-20 10:21:49 -0400

    Added right side design

--- app/page.js
+++ app/page.js
@@ -84,6 +84,7 @@ export default function Home() {
           <option value='8footDesignRight-6.png'>8footDesignRight-6.png</option>
           <option value='8footDesignRight-7.png'>8footDesignRight-7.png</option>
           <option value='8footDesignRight-8.png'>8footDesignRight-8.png</option>
+          <option value='8footDesignRight-9.png'>8footDesignRight-9.png</option>
         </select>
       </nav>
 
